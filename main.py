0dd1a8663d51497a0868a2d259478861eda0ecdb
innovationsquared <pettiscd@appstate.edu>
2025-04-12 14:28:44 -0400

    chess board

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ import board
 
 SCREEN_WIDTH = 940
 SCREEN_HEIGHT =  540
-
+pygame.init()
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Pygame")
 clock = pygame.time.Clock()
 
 run = True
+
 while run:
 
-    board.draw_board(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT)
+    
 
     # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+
+    SCREEN.fill('blue')
+    #pygame.draw.rect(SCREEN, 'white', pygame.Rect(30,30,60,60))
+    board.draw_board(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT)   
+    pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
