c1195ccca6273972a31db9d53d67c295c23f969b
sylviavitner <vitnersylvia@gmail.com>
2025-04-12 16:13:22 -0400

    piece class

---
+++ moves.py
@@ -0,0 +1,37 @@
+import pygame
+
+white_pieces = {'pawn1': 'a2',
+                'pawn2': 'b2',
+                'pawn3': 'c2',
+                'pawn4': 'd2',
+                'pawn5': 'e2',
+                'pawn6': 'f2',
+                'pawn7': 'g2',
+                'pawn8': 'h2',
+                'left_rook': 'a1',
+                'right_rook': 'h1',
+                'left_knight': 'b1',
+                'right_knight': 'g1',
+                'left_bishop': 'c1',
+                'right_bishop': 'f1',
+                'queen': 'd1',
+                'king': 'e1'
+                }   
+
+black_pieces = {'pawn1': 'a7',
+                'pawn2': 'b7',
+                'pawn3': 'c7',
+                'pawn4': 'd7',
+                'pawn5': 'e7',
+                'pawn6': 'f7',
+                'pawn7': 'g7',
+                'pawn8': 'h7',
+                'left_rook': 'a8',
+                'right_rook': 'h8',
+                'left_knight': 'b8',
+                'right_knight': 'g8',
+                'left_bishop': 'c8',
+                'right_bishop': 'f8',
+                'queen': 'd8',
+                'king': 'e8'
+                }   
---
+++ piece.py
@@ -0,0 +1,22 @@
+import pygame
+import moves
+
+# Pieces and corresponding locations
+
+white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+                   (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+black_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7),
+                   (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+
+# Piece class
+
+class Piece():
+    def __init__(self, type, color, image):
+        self.type = type
+        self.color = color
+        self.image = pygame.image.load(image).convert_alpha()
+        self.location = self.getStartingLocation()
