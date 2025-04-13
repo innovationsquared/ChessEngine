4e81adb6f51dbd4b216e7554d293906b971283cc
sylviavitner <vitnersylvia@gmail.com>
2025-04-13 05:13:00 -0400

    pawn logic

--- board.py
+++ board.py
@@ -10,9 +10,8 @@ class Board:
     def __init__(self):
         self.squares = [[0,0,0,0,0,0,0,0] for col in range(8)] 
         self.create()
-        self.add_pieces('white')
-        self.add_pieces('black')
-    
+        self.add_pieces()
+        
     # Calculate all valid moves of a specific piece at a position.
     def calculate_moves(self, piece, row, col):
         
@@ -25,17 +24,45 @@ class Board:
                 steps = 2
             
             # potential vertical moves
+            start = row + piece.dir 
+            # move in the direction (-1 or 1) the number of steps
+            # add 1 to account for excluded (in range)
+            end = row + (piece.dir * (1 + steps))
+            for move_row in range(start, end, piece.dir):
+                # check to make sure the square is in range and empty
+                if Square.in_range(move_row):
+                    if self.squares[move_row][col].is_empty():
+                        # create initial and target moves
+                        initial = Square(move_row, col)
+                        target = Square(move_row, col)
+                        move = Move(initial, target)
+                        piece.add_move(move)
+                    # blocked by another piece
+                    else:
+                        break
+                # not in range
+                else:
+                    break
             
+            # diagonal moves
+            move_row = row + piece.dir
+            move_cols = [col-1,col+1]
+            
+            for move_col in move_cols:
+                # check for in range and has an enemy on the target square
+                if Square.in_range(move_row, move_col):
+                    if self.squares[move_row][move_col].has_rival(piece.color):
+                        initial = Square(move_row, move_col)
+                        target = Square(move_row, move_col)
+                        move = Move(initial,target)
+                        piece.add_move(move)
 
             
-
-
 
         # Knight moves
         def knight_moves():
             potential_moves = [(row-2,col+1),(row+2,col+1),(col+2,row-1),(row-2,col-1),
                      (row+1,col+2),(row+1,col-2),(row-1,col+2),(row-1,col-2)]
-            
 
             for potential_move in potential_moves:
                 potential_move_row, potential_move_col = potential_move
