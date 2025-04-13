c3a4f728c39d7517fe70abfc3a7925a51d9212b0
innovationsquared <pettiscd@appstate.edu>
2025-04-12 21:31:44 -0400

    help

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ SCREEN_HEIGHT = 400
 
 pygame.init()
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Cockfish")
-clock = pygame.time.Clock()
+pygame.display.set_caption("Hackfish")
+#clock = pygame.time.Clock()
 
 run = True
 
@@ -18,10 +18,8 @@ while run:
             run = False
 
     SCREEN.fill((145, 135, 97))
-    #pygame.draw.rect(SCREEN, 'white', pygame.Rect(30,30,60,60))
     board.draw_board(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT)   
-    pygame.display.flip()
-    clock.tick(60)
+    pygame.display.update()
 
 pygame.quit()
 
